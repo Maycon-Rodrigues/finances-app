c85dd0aeb997e4c54e83ec6fab0c5288b40a240d
Maycon Rodrigues <mrtdb14@hotmail.com>
2020-07-13 14:49:44 -0300

    change schema to required filds

--- models/TransactionModel.js
+++ models/TransactionModel.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 const schema = mongoose.Schema({
-  description: String,
+  description: { type: String, required: true },
   value: Number,
-  category: String,
-  year: Number,
-  month: Number,
-  day: Number,
-  yearMonth: String,
-  yearMonthDay: String,
-  type: String,
+  category: { type: String, required: true },
+  year: { type: Number, required: true },
+  month: { type: Number, required: true },
+  day: { type: Number, required: true },
+  yearMonth: { type: String, required: true },
+  yearMonthDay: { type: String, required: true },
+  type: { type: String, required: true },
 });
 
 const TransactionModel = mongoose.model('transaction', schema, 'transaction');
