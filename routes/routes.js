8f671d9c5fcf8a3c537848db2785544aee416872
Maycon Rodrigues <mrtdb14@hotmail.com>
2020-07-13 18:13:14 -0300

    change router name and add routers

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,10 @@
-const express = require('express');
-const transactionRouter = express.Router();
+const router = require('express').Router();
 
-module.exports = transactionRouter;
+const service = require('../services/transactionService');
+
+router.get('/', service.getAll);
+router.post('/create', service.create);
+router.patch('/update/:id', service.update);
+router.delete('/delete/:id', service.delete);
+
+module.exports = router;
