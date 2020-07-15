91848bb9e41037f5679e3e7b0ab49e6c9728d449
Maycon Rodrigues <mrtdb14@hotmail.com>
2020-07-15 11:38:47 -0300

    add components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import TransactionList from './components/Transaction/TransactionList';
+import Header from './components/Header/Header';
+
 export default function App() {
-  return <h1>Desafio Final do Bootcamp Full Stack</h1>;
+  return (
+    <div className="container">
+      <Header />
+      <TransactionList />
+    </div>
+  );
 }
