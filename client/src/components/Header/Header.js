494286dc24c8da1af3ef680fb8e50182a099c25d
Maycon Rodrigues <mrtdb14@hotmail.com>
2020-07-15 11:35:34 -0300

    create header component

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './style.css';
+
+export default function Header() {
+  return (
+    <div className="center">
+      <h3>Bootcamp Full Stack - Desafio Final</h3>
+      <span className="subtitle">Controle Financeiro Pessoal</span>
+    </div>
+  );
+}
