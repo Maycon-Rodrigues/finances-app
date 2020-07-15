d72eee6695fb2ff5d7a437b9be2d9980b35d2f08
Maycon Rodrigues <mrtdb14@hotmail.com>
2020-07-15 11:58:51 -0300

    add spinner when data is empty

--- client/src/components/Transaction/TransactionList.js
+++ client/src/components/Transaction/TransactionList.js
@@ -3,14 +3,17 @@ import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
 import Card from './Card/Card';
 import Info from './Info/Info';
+import Spinner from '../Spinner/Spinner';
 
 export default function TransactionList() {
   const [transactions, setTransactions] = useState([]);
   const [releases, setReleases] = useState(0);
 
+  const period = '2019-03';
+
   useEffect(() => {
     const getData = async () => {
-      const result = await (await api.get('/api/transaction?period=2020-07'))
+      const result = await (await api.get(`/api/transaction?period=${period}`))
         .data;
 
       setTransactions(result.transactions);
@@ -23,6 +26,7 @@ export default function TransactionList() {
   return (
     <div>
       <Info length={releases} data={transactions} />
+      {!transactions.length && <Spinner />}
       <Card data={transactions} />
     </div>
   );
