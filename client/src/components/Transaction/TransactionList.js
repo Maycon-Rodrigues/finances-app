63364b23bd586325369e47434d145cc265b7e04a
Maycon Rodrigues <mrtdb14@hotmail.com>
2020-07-16 14:32:56 -0300

    add input component and context api

--- client/src/components/Transaction/TransactionList.js
+++ client/src/components/Transaction/TransactionList.js
@@ -1,13 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, createContext } from 'react';
 
 import api from '../../services/api';
+
 import Card from './Card/Card';
 import Info from './Info/Info';
 import Spinner from '../Spinner/Spinner';
+import InputFilter from '../InputFilter/InputFilter';
+
+export const TransactionContext = createContext([]);
 
 export default function TransactionList() {
   const [transactions, setTransactions] = useState([]);
   const [releases, setReleases] = useState(0);
+  const [input, setInput] = useState('');
 
   const period = '2019-03';
 
@@ -23,11 +28,24 @@ export default function TransactionList() {
     getData();
   }, []);
 
+  const handleInputChange = (newValue) => {
+    setInput(newValue);
+  };
+
+  const filter = transactions.filter((d) =>
+    d.description.toLowerCase().includes(input)
+  );
+
   return (
     <div>
-      <Info length={releases} data={transactions} />
-      {!transactions.length && <Spinner />}
-      <Card data={transactions} />
+      <TransactionContext.Provider value={filter}>
+        <InputFilter currentValue={input} onInputChange={handleInputChange} />
+
+        <Info length={releases} />
+
+        {!transactions.length && <Spinner />}
+        <Card />
+      </TransactionContext.Provider>
     </div>
   );
 }
