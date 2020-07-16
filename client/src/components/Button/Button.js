2c3eb1ab210508f79a35bf0985c6676af64d60b9
Maycon Rodrigues <mrtdb14@hotmail.com>
2020-07-16 14:31:01 -0300

    create button component

---
+++ client/src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Button() {
+  return (
+    <button
+      type="submit"
+      className="waves-effect waves-light green accent-4 btn"
+    >
+      Novo Lançamento
+    </button>
+  );
+}
