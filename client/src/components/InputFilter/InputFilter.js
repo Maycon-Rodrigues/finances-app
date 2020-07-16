63cad83595e6adb687720e00efb07eeb6e0b6192
Maycon Rodrigues <mrtdb14@hotmail.com>
2020-07-16 14:31:41 -0300

    create input component to filter the finances

---
+++ client/src/components/InputFilter/InputFilter.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './style.css';
+
+import Button from '../Button/Button';
+
+export default function InputFilter({ onInputChange, currentValue }) {
+  const handleChange = (event) => {
+    const newValue = event.target.value.toLowerCase();
+
+    onInputChange(newValue);
+  };
+
+  return (
+    <div className="inputRow">
+      <div className="input-field">
+        <i className="material-icons prefix active">search</i>
+        <label className="active" htmlFor="inputFilter">
+          Buscar Lançamento
+        </label>
+        <input
+          autoFocus
+          id="inputFilter"
+          placeholder="Compras em padaria"
+          type="text"
+          className="validate"
+          value={currentValue}
+          onChange={handleChange}
+        />
+      </div>
+
+      <Button />
+    </div>
+  );
+}
