b0d514823fa92e50536cff23f95c4140620f6b75
Maycon Rodrigues <mrtdb14@hotmail.com>
2020-07-23 15:56:55 -0300

    add context api to open new lauch

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TransactionContext } from '../Transaction/TransactionList';
 
 export default function Button() {
+  const context = useContext(TransactionContext);
+
   return (
     <button
+      onClick={context}
       type="submit"
       className="waves-effect waves-light green accent-4 btn"
     >
